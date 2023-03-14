ad080c604d6bc12a860878dd11514ef6e7982fd0
kisman <81470009+kisman2000@users.noreply.github.com>
2023-03-14 17:50:41 +0300

    polygons + code improvements

--- renderer.py
+++ renderer.py
@@ -9,86 +9,127 @@ KEY_LSHIFT = 16
 
 fov = 2
 a = (fov / 180) * math.pi
-
+ 
 x0 = 0.5
 y0 = 0.5
-
+ 
 aspectX = 4
 aspectY = 4
 aspectZ = min(aspectX, aspectY) * 2
-
+ 
 cameraX = 0
 cameraY = 0
 cameraZ = 40
-
-objects = [ [ None, 0, 0, 0, 0, 0, 0 ] ]
-
+ 
+speedX = 5
+speedY = 5
+speedZ = 5
+ 
+class Point :
+    def __init__(self, x, y, z, projectX, projectY) :
+        self.x = x
+        self.y = y
+        self.z = z
+        self.projectX = projectX
+        self.projectY = projectY
+ 
+ 
+class LineObject :
+    def __init__(self, line, point1, point2) :
+        self.line = line
+        self.point1 = point1
+        self.point2 = point2
+ 
+class PolygonObject :
+    def __init__(self, polygon, points, color) :
+        self.polygon = polygon
+        self.points = points
+        self.color = color
+ 
+ 
+lines = list()
+polygons = list()
+ 
 def modify(point) : 
-    return [ point[0] + x0 * graph.DEF_GRAPH_WIDTH, point[1] + y0 * graph.DEF_GRAPH_HEIGHT ]
-
+    return [ point.projectX + x0 * graph.DEF_GRAPH_WIDTH, point.projectY + y0 * graph.DEF_GRAPH_HEIGHT ]
+ 
 def project(x, y, z) :
     x = (x + cameraX) / ((z / aspectZ + cameraZ) * math.tan(a / aspectX))
     y = (y + cameraY) / ((z / aspectZ + cameraZ) * math.tan(a / aspectY))
-
+ 
     return [ x, y ]
-
+ 
 def point(x, y, z) :
-    return project(x, y, z)
-
+    projection = project(x, y, z)
+    return Point(x, y, z, projection[0], projection[1])
+ 
 def moveTo(point) :
     graph.moveTo(modify(point)[0], modify(point)[1])
-
+ 
 def lineTo(point) :
     return graph.lineTo(modify(point)[0], modify(point)[1])
-
+ 
 def line(point1, point2) :
     return graph.line(modify(point1)[0], modify(point1)[1], modify(point2)[0], modify(point2)[1])
 
+def polygon(points) :
+    points0 = [ ]
+
+    for point in points :
+        points0.append((modify(point)[0], modify(point)[1]))
+
+    points0.append((modify(points[0])[0], modify(points[0])[1]))
+
+    return graph.polygon(points0)
+ 
 def update() :
-    for pair in objects :
-        object1 = pair[0]
-        x1 = pair[1]
-        y1 = pair[2]
-        z1 = pair[3]
-        x2 = pair[4]
-        y2 = pair[5]
-        z2 = pair[6]
-        
-        point1 = point(x1, y1, z1)
-        point2 = point(x2, y2, z2)
+    for polygon0 in polygons :
+        points0 = list()
 
-        graph.deleteObject(object1)
+        for point1 in polygon0.points :
+            points0.append(point(point1.x, point1.y, point1.z))
 
-        objects.remove(pair)
+        polygon0.points = points0
 
-        object2 = line(point1, point2)
+        if polygon0.polygon != None :
+            graph.deleteObject(polygon0.polygon)
+ 
+        graph.brushColor(polygon0.color)
+ 
+        polygon0.polygon = polygon(polygon0.points)
 
-        objects.append( [ object2, x1, y1, z1, x2, y2, z2 ] )
-
+    for line0 in lines :
+        point1 = point(line0.point1.x, line0.point1.y, line0.point1.z)
+        point2 = point(line0.point2.x, line0.point2.y, line0.point2.z)
+ 
+        graph.deleteObject(line0.line)
+ 
+        line0.line = line(point1, point2)
+ 
 def key(event) :
     global cameraX, cameraY, cameraZ
-
+ 
     if(event.keycode == KEY_W) :
-        cameraZ += 1
-
+        cameraZ += speedZ
+ 
     if(event.keycode == KEY_S) :
-        cameraZ -= 1
-
+        cameraZ -= speedZ
+ 
     if(event.keycode == KEY_A) :
-        cameraX -= 1
-
+        cameraX -= speedX
+ 
     if(event.keycode == KEY_D) :
-        cameraX += 1
-
+        cameraX += speedX
+ 
     if(event.keycode == graph.VK_SPACE) :
-        cameraY -= 1
-
+        cameraY -= speedY
+ 
     if(event.keycode == KEY_LSHIFT) :
-        cameraY += 1
-
+        cameraY += speedY
+ 
     update()
-
-def aabb(minX, minY, minZ, maxX, maxY, maxZ) :
+ 
+def aabb(minX, minY, minZ, maxX, maxY, maxZ, color) :
     point1 = point(minX, minY, minZ)
     point2 = point(maxX, minY, minZ)
     point3 = point(maxX, minY, maxZ)
@@ -97,10 +138,10 @@ def aabb(minX, minY, minZ, maxX, maxY, maxZ) :
     point6 = point(maxX, maxY, minZ)
     point7 = point(maxX, maxY, maxZ)
     point8 = point(minX, maxY, maxZ)
-
+ 
     line1 = line(point1, point2)
     line2 = line(point2, point3)
-    line3 = line(point3, point3)
+    line3 = line(point3, point4)
     line4 = line(point4, point1)
     line5 = line(point5, point6)
     line6 = line(point6, point7)
@@ -111,23 +152,37 @@ def aabb(minX, minY, minZ, maxX, maxY, maxZ) :
     line11 = line(point3, point7)
     line12 = line(point4, point8)
 
-    objects.append( [ line1, minX, minY, minZ, maxX, minY, minZ ] )
-    objects.append( [ line2, maxX, minY, minZ, maxX, minY, maxZ ] )
-    objects.append( [ line3, maxX, minY, maxZ, minX, minY, maxZ ] )
-    objects.append( [ line4, minX, minY, maxZ, minX, minY, minZ ] )
-    objects.append( [ line5, minX, maxY, minZ, maxX, maxY, minZ ] )
-    objects.append( [ line6, maxX, maxY, minZ, maxX, maxY, maxZ ] )
-    objects.append( [ line7, maxX, maxY, maxZ, minX, maxY, maxZ ] )
-    objects.append( [ line8, minX, maxY, maxZ, minX, maxY, minZ ] )
-    objects.append( [ line9, minX, minY, minZ, minX, maxY, minZ ] )
-    objects.append( [ line10, maxX, minY, minZ, maxX, maxY, minZ ] )
-    objects.append( [ line11, maxX, minY, maxZ, maxX, maxY, maxZ ] )
-    objects.append( [ line12, minX, minY, maxZ, minX, maxY, maxZ ] )
+    polygon1 = polygon([ point1, point2, point3, point4 ])
+    polygon2 = polygon([ point5, point6, point7, point8 ])
+    polygon3 = polygon([ point1, point2, point6, point5 ])
+    polygon4 = polygon([ point3, point4, point8, point7 ])
+    polygon5 = polygon([ point1, point4, point8, point5 ])
+    polygon6 = polygon([ point2, point3, point7, point6 ])
+ 
+    lines.append(LineObject(line1, point1, point2))
+    lines.append(LineObject(line2, point2, point3))
+    lines.append(LineObject(line3, point3, point4))
+    lines.append(LineObject(line4, point4, point1))
+    lines.append(LineObject(line5, point5, point6))
+    lines.append(LineObject(line6, point6, point7))
+    lines.append(LineObject(line7, point7, point8))
+    lines.append(LineObject(line8, point8, point5))
+    lines.append(LineObject(line9, point1, point5))
+    lines.append(LineObject(line10, point2, point6))
+    lines.append(LineObject(line11, point3, point7))
+    lines.append(LineObject(line12, point4, point8))
 
-def cube(centerX, centerY, centerZ, size) :
-    aabb(centerX - size, centerY - size, centerZ - size, centerX + size, centerY + size, centerZ + size)
-
-def pyramid(centerX, centerY, centerZ, height, size) :
+    polygons.append(PolygonObject(polygon1, [ point1, point2, point3, point4 ], color))
+    polygons.append(PolygonObject(polygon2, [ point5, point6, point7, point8 ], color))
+    polygons.append(PolygonObject(polygon3, [ point1, point2, point6, point5 ], color))
+    polygons.append(PolygonObject(polygon4, [ point3, point4, point8, point7 ], color))
+    polygons.append(PolygonObject(polygon5, [ point1, point4, point8, point5 ], color))
+    polygons.append(PolygonObject(polygon6, [ point2, point3, point7, point6 ], color))
+ 
+def cube(centerX, centerY, centerZ, size, color) :
+    aabb(centerX - size, centerY - size, centerZ - size, centerX + size, centerY + size, centerZ + size, color)
+ 
+def pyramid(centerX, centerY, centerZ, height, size, color) :
     minX = centerX - size
     minZ = centerZ - size
     maxX = centerX + size
@@ -149,29 +204,44 @@ def pyramid(centerX, centerY, centerZ, height, size) :
     line6 = line(point2, point5)
     line7 = line(point3, point5)
     line8 = line(point4, point5)
- 
-    objects.append( [ line1, minX, centerY, minZ, maxX, centerY, minZ ] )
-    objects.append( [ line2, maxX, centerY, minZ, maxX, centerY, maxZ ] )
-    objects.append( [ line3, maxX, centerY, maxZ, minX, centerY, maxZ ] )
-    objects.append( [ line4, minX, centerY, maxZ, minX, centerY, minZ ] )
-    objects.append( [ line5, minX, centerY, minZ, centerX, centerY - height, centerZ ] )
-    objects.append( [ line6, maxX, centerY, minZ, centerX, centerY - height, centerZ ] )
-    objects.append( [ line7, maxX, centerY, maxZ, centerX, centerY - height, centerZ ] )
-    objects.append( [ line8, minX, centerY, maxZ, centerX, centerY - height, centerZ ] )
 
+    polygon1 = polygon([ point1, point2, point3, point4 ])
+    polygon2 = polygon([ point1, point2, point5 ])
+    polygon3 = polygon([ point2, point3, point5 ])
+    polygon4 = polygon([ point3, point4, point5 ])
+    polygon5 = polygon([ point4, point1, point5 ])
+ 
+    lines.append(LineObject(line1, point1, point2))
+    lines.append(LineObject(line2, point2, point3))
+    lines.append(LineObject(line3, point3, point4))
+    lines.append(LineObject(line4, point4, point1))
+    lines.append(LineObject(line5, point1, point5))
+    lines.append(LineObject(line6, point2, point5))
+    lines.append(LineObject(line7, point3, point5))
+    lines.append(LineObject(line8, point4, point5))
+ 
+    polygons.append(PolygonObject(polygon1, [ point1, point2, point3, point4 ], color))
+    polygons.append(PolygonObject(polygon2, [ point1, point2, point5 ], color))
+    polygons.append(PolygonObject(polygon3, [ point2, point3, point5 ], color))
+    polygons.append(PolygonObject(polygon4, [ point3, point4, point5 ], color))
+    polygons.append(PolygonObject(polygon5, [ point4, point1, point5 ], color))
+ 
 graph.onKey(key)
 graph.onTimer(update, 1)
-
+ 
 graph.penColor("black")
-
-aabb(-15, 0, -15, 15, 30, 15)
-pyramid(0, 0, 0, 30, 50)
-pyramid(0, -30, 0, 30, 30)
-pyramid(0, -60, 0, 30, 20)
-
+ 
+ 
+aabb(-15, 0, -15, 15, 30, 15, "brown")
+pyramid(0, 0, 0, 30, 50, "green")
+pyramid(0, -30, 0, 30, 30, "green")
+pyramid(0, -60, 0, 30, 20, "green")
+ 
+ 
 # cube(0, 0, 1, 30)
 # cube(0, 60, 1, 30)
 # cube(80, 0, 1, 40)
-
-
+# pyramid(-80, 40, 1, 80, 40)
+ 
+ 
 graph.run()
